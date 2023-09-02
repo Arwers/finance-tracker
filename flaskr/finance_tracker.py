4ee82319366fe1420f17278b1b995a52b6a03b00
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-02 22:24:47 +0200

    added better examples in list of expenses

--- flaskr/finance_tracker.py
+++ flaskr/finance_tracker.py
@@ -7,15 +7,15 @@ app = Flask(__name__, template_folder="templates")
 
 # list of dictionaries of expenses
 expenses = [
-    {"name": "wydatki"},
-    {"name": "wydatki2"},
-    {"name": "wydatki3"},
-    ]
+    {"name": "expense1", "cost": 300, "date": "11.11.2023"},
+    {"name": "expense2", "cost": 150, "date": "02.11.2020"},
+    {"name": "expense3", "cost": 50, "date": "14.11.203"},
+]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", expenses = expenses)
+    return render_template("index.html", expenses=expenses)
 
 
 if __name__ == "__main__":
