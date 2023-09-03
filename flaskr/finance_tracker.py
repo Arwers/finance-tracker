05c77a1865829ce807ee76584487c1bb3cdec662
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-03 21:46:10 +0200

    Bug fixed: cost was not a number

--- flaskr/finance_tracker.py
+++ flaskr/finance_tracker.py
@@ -12,14 +12,13 @@ expenses = [
     {"name": "expense3", "cost": 50, "date": "14.11.203"},
 ]
 
-# total cost of all expenses
-total_cost = 0
-for item in expenses:
-    total_cost += item["cost"]
-
-
 @app.route("/")
 def index():
+    # total cost of all expenses
+    total_cost = 0
+    for item in expenses:
+        total_cost += item["cost"]
+
     return render_template("index.html", expenses=expenses, total_cost=total_cost)
 
 
@@ -28,7 +27,7 @@ def add():
     name = request.form["name"]
     cost = request.form["cost"]
     date = request.form["date"]
-    expenses.append({"name": name, "cost": cost, "date": date})
+    expenses.append({"name": name, "cost": int(cost), "date": date})
     return redirect(url_for("index"))
 
 
