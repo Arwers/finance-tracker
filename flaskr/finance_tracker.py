f37586ed9d3c47ac270f561fd33eaf81f6600e90
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-04 14:47:54 +0200

    feat: remove total_cost from index fun

--- flaskr/finance_tracker.py
+++ flaskr/finance_tracker.py
@@ -11,23 +11,23 @@ expenses = [
     {"name": "expense2", "cost": 150, "date": "02.11.2020"},
     {"name": "expense3", "cost": 50, "date": "14.11.203"},
 ]
+# total cost of all expenses
+total_cost = 0
+for item in expenses:
+    total_cost += item["cost"]
+
 
 @app.route("/")
 def index():
-    # total cost of all expenses
-    total_cost = 0
-    for item in expenses:
-        total_cost += item["cost"]
-
     return render_template("index.html", expenses=expenses, total_cost=total_cost)
 
 
 @app.route("/add", methods=["POST"])
 def add():
     name = request.form["name"]
-    cost = request.form["cost"]
+    cost = int(request.form["cost"])
     date = request.form["date"]
-    expenses.append({"name": name, "cost": int(cost), "date": date})
+    expenses.append({"name": name, "cost": cost, "date": date})
     return redirect(url_for("index"))
 
 
@@ -36,10 +36,14 @@ def delete(index):
     del expenses[index]
     return redirect(url_for("index"))
 
-@app.route("/update")
+
+@app.get("/update")
 def update():
-    ...
-    return redirect(url_for("index"))
+    global gen_total  # get the global iterator
+
+    # return the next value in iterator
+    return str(next(gen_total))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
