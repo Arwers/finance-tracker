2996688c254a1dbaefca84bf6c56b79398a3c88a
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-08 00:26:58 +0200

    style: add comments next to items to fix

--- flaskr/homepage/__init__.py
+++ flaskr/homepage/__init__.py
@@ -1,2 +1,13 @@
+from flaskr.homepage.utils import count_money_left
+from flaskr.__init__ import app
 
+
+def count_money_left(limit, total):
+    temp = limit - total
+    if temp < 0:
+        return temp
+    else:
+        return 0
+
+# !
 app.jinja_env.globals.update(count_money_left = count_money_left)
