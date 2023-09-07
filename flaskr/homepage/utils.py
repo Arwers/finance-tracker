3f168e3b98fa5447b528e799b6734aabd2747584
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-08 00:13:12 +0200

    refactor: move count_money_left to utils

--- flaskr/homepage/utils.py
+++ flaskr/homepage/utils.py
@@ -1,6 +1,13 @@
-
 def total_cost_count(expenses):
     total_cost = 0
     for item in expenses:
         total_cost += item["cost"]
     return total_cost
+
+
+def count_money_left(limit, total):
+    temp = limit - total
+    if temp < 0:
+        return temp
+    else:
+        return 0
