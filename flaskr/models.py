f2e156528b5b6c627dfe26990408d9d35b23276f
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-15 09:58:34 +0200

    feat: add columns to Expenses model

--- flaskr/models.py
+++ flaskr/models.py
@@ -2,4 +2,12 @@ from . import db
 
 
 class Expenses(db.Model):
-    ...
+    # keys
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, foreign_key=True)
+    
+    # values
+    name = db.Column(db.String(64))
+    cost = db.Column(db.Numeric(10, 2))
+    date = db.Column(db.Date())
+    category = db.Column(db.String(32))
