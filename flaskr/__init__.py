ed43ef008f54ee2c981d1de9e25ef0a1c13d0a08
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-15 09:45:54 +0200

    feat: add database initialization

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .homepage.views import homepage
+from flask_sqlalchemy import SQLAlchemy
 
-
+# app
 app = Flask(__name__)
 app.config.from_object('config')
 
 #blueprints
 app.register_blueprint(homepage)
+
+# database
+db = SQLAlchemy(app)
+from models import Payments
