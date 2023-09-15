752eae8f1c2fe718180c4e9e118b8e77a69fa10b
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-15 10:25:47 +0200

    feat: add Users table

--- flaskr/models.py
+++ flaskr/models.py
@@ -13,3 +13,14 @@ class Expenses(db.Model):
     cost = db.Column(db.Numeric(10, 2))
     date = db.Column(db.Date())
     category = db.Column(db.String(32))
+
+
+class Users(db.Model):
+    __tablename__ = "Users"
+
+    # keys
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    # values
+    username = db.Column(db.String(32))
+    password = db.Column(db.String(32))
