63ca7fcebeaeca54677678bd8da0a2990f4532a1
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-17 10:29:16 +0200

    fix: create_app returns app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 
 
-# app
-def create_app():
+def create_app(config_filename):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object("config")
-    app.config.from_pyfile("config.py")
+    app.config.from_pyfile(config_filename)
 
     # database
     from .models import db
-    db.init_app()
+    db.init_app(app)
 
     # blueprints
     from .homepage.views import homepage
     app.register_blueprint(homepage)
+    
+    return app
