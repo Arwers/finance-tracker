c6c151e4023b32d6d3cb331d1f1914b307b58e86
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-17 10:30:13 +0200

    feat: add db import

--- flaskr/homepage/views.py
+++ flaskr/homepage/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .utils import *
-
+from ..models import db
 
 homepage = Blueprint(
     "homepage", __name__, template_folder="templates", static_folder="static"
