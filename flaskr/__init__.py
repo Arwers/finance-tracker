beb9d802db07ae52b554d0cb8cd4c1c2ed80d573
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-17 17:03:06 +0200

    feat: add default argument for config_filename

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-def create_app(config_filename):
+def create_app(config_filename="config.py"):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object("config")
     app.config.from_pyfile(config_filename)
