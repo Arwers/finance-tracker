5a41fc04e10f0b6e308adf4449f1c2ab98fe9690
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-23 11:33:43 +0200

    feat: settings button redirects to settings blueprint

--- flaskr/settings/views.py
+++ flaskr/settings/views.py
@@ -1,7 +1,7 @@
 # Standard library imports
 
 # Third-party imports
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
 
 # Local file imports
 
@@ -10,4 +10,11 @@ settings = Blueprint(
     "settings", __name__, template_folder="templates", static_folder="static", static_url_path="/flaskr/settings"
 )
 
+@settings.route("/settings")
+def index():
+        return render_template(
+        "settings.html",
+        limit=current_app.limit,
+        categories=current_app.categories,
+    )
 
