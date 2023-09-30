4f8714ec5963d3a6c9219c917459a4058eb499e6
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-30 15:06:46 +0200

    refactor: final changes to match new location

--- flaskr/home/views.py
+++ flaskr/home/views.py
@@ -20,7 +20,7 @@ all_costs = set_all_costs(all_costs, temp_expenses)
 @home.route("/")
 def index():
     return render_template(
-        "index.html",
+        "home/index.html",
         expenses=Expenses.query.all(),
         all_costs=all_costs,
         limit=current_app.limit,
