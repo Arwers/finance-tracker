7570e9c8ead7d907e7b5cecd748ad58d3260b9e1
Marcin Fortuna <marcin.fortuna@onet.eu>
2023-09-30 16:53:18 +0200

    feat: add insights blueprint

---
+++ flaskr/insights/views.py
@@ -0,0 +1,20 @@
+# Standard library imports
+
+# Third-party imports
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+
+# Local file imports
+
+
+insights = Blueprint(
+    "insights", __name__, template_folder="templates", static_folder="static",
+)
+
+@insights.route("/insights")
+def index():
+    return render_template(
+        "insights/index.html",
+        limit=current_app.limit,
+        categories=current_app.categories,
+        currency=current_app.currency,
+    )
